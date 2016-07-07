bac0c257f6dfe1dc1a4ae53800c419a5ed1c44e5
daryawood <darya0veretennikova@gmail.com>
2016-07-08 00:34:02 +0300

    first working version of website aggregator

--- client/main.js
+++ client/main.js
@@ -3,6 +3,57 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+
+//Routing
+
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+  this.render('navbar', {
+    to: 'navbar'
+  });
+  this.render('websiteMain', {
+    to: 'main'
+  });
+});
+
+// Router.route('/images', function () {
+//   this.render('navbar', {
+//     to: 'navbar'
+//   });
+//   this.render('images', {
+//     to: 'main'
+//   });
+// });
+//
+Router.route(':_id', function () {
+  this.render('navbar', {
+    to: 'navbar'
+  });
+  this.render('detailPage', {
+    to: 'main',
+    data: function() {
+      return Websites.findOne({_id: this.params._id});
+    }
+  });
+});
+
+//end Routing
+
+//comments
+
+Comments.ui.config({
+   template: 'bootstrap',
+   defaultAvatar: function() {
+     var size = Math.round(Math.random() * 100);
+     return 'http://placekitten.com/' + size +'/' + size +'/';
+   }()
+});
+
+// /end comments
+
 /////
 // template helpers
 /////
@@ -10,7 +61,9 @@ import './main.html';
 // helper function that returns all available websites
 Template.website_list.helpers({
   websites:function(){
-    return Websites.find({});
+    return Websites.find({}, {sort: {rating: -1}});
+
+    //return Images.find({}, { sort: { createdOn: -1, rating: -1 }, limit: Session.get('imageLimit') });
   }
 });
 
@@ -20,10 +73,23 @@ Template.website_list.helpers({
 /////
 
 Template.website_item.events({
-  "click .js-upvote":function(event){
+  "click .js-upvote":function(e){
     // example of how you can access the id for the website in the database
     // (this is the data context for the template)
     var website_id = this._id;
+
+    console.log(Meteor)
+
+    Websites.update(
+      website_id,
+      {$set: {
+        rating: ++this.rating,
+        upvotedCounter: ++this.upvotedCounter
+      }}
+    );
+
+    console.log(this)
+
     console.log("Up voting website with id "+website_id);
     // put the code in here to add a vote to a website!
 
@@ -34,6 +100,15 @@ Template.website_item.events({
     // example of how you can access the id for the website in the database
     // (this is the data context for the template)
     var website_id = this._id;
+
+    Websites.update(
+      website_id,
+      {$set: {
+        rating: --this.rating,
+        downvotedCounter: ++this.downvotedCounter
+      }}
+    );
+
     console.log("Down voting website with id "+website_id);
 
     // put the code in here to remove a vote from a website!
@@ -50,6 +125,32 @@ Template.website_form.events({
 
     // here is an example of how to get the url out of the form:
     var url = event.target.url.value;
+    var title = event.target.title.value;
+    var description = event.target.description.value;
+
+    if (url && description) {
+      Websites.insert({
+        url: url,
+        title: title || 'Noname title',
+        description: description,
+        createdOn: new Date(),
+        createdBy: Meteor.user()._id,
+        rating: 0,
+        upvotedCounter: 0,
+        downvotedCounter: 0
+      })
+
+
+      $('#website_form')
+        .find('input').val('');
+      $('#website_form').toggle('slow');
+    } else {
+      alert('Add and url and a description to add new website!')
+    }
+
+
+
+    console.log(url, title, description)
     console.log("The url they entered is: "+url);
 
     //  put your website saving code in here!
